3cebcf91372bae7338406f0a8179b4e32863c318
Abeeq <l164253@lhr.nu.edu.pk>
2021-02-24 04:57:47 +0500

    first commit

---
+++ Assets/Flappy Bird Assets/Scripts/Bird.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour 
+{
+	public float upForce;			
+	private bool isDead = false;	
+
+	private Animator anim;					
+	private Rigidbody2D rb;				
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+		
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		
+		if (isDead == false) 
+		{
+			
+			if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space")) 
+			{
+				
+				anim.SetTrigger("Flap");
+				
+				rb.velocity = Vector2.zero;
+				
+				rb.AddForce(new Vector2(0, upForce));
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		
+		rb.velocity = Vector2.zero;
+		
+		isDead = true;
+		
+		anim.SetTrigger ("Die");
+		GameControl.instance.BirdDied (); 
+	}
+}
